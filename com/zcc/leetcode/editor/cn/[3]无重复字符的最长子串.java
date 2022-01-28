1631a6d11f3f033a7a636d4259b81a344aefef42
zhangchangchun <13122310326@163.com>
2022-01-28 20:18:10 +0800

    commit

---
+++ com/zcc/leetcode/editor/cn/[3]无重复字符的最长子串.java
@@ -0,0 +1,14 @@
+package zcc.leetcode.editor.cn;
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if(s.length()<=1){
+            return s.length();
+        }
+        Map<>
+        return 1;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
