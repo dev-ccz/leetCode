b05732cc9432e836e547e74229881d7df676b0a3
zhangchangchun <13122310326@163.com>
2022-04-12 18:43:13 +0800

    第四题和第五题

---
+++ com/zcc/初级算法/_4/Solution_4.java
@@ -0,0 +1,24 @@
+package zcc.初级算法._4;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author zcc  2022/4/11 22:00
+ */
+public class Solution_4 {
+
+    public static void main(String[] args) {
+        Solution_4 solution_4 = new Solution_4();
+        System.out.println(solution_4.containsDuplicate(new int[]{1,2,3,4}));
+    }
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> integers = new HashSet<>();
+        for (int num : nums) {
+            if(!integers.add(num)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
